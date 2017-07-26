ca8de6216addf6891cca4ce3c6de6b8f5522ec1c
Yedidya Kennard <yedidyak@wix.com>
2017-07-26 09:42:40 +0300

    Added check for left items, exported no yellow box disable rule in index

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 module.exports = {
   rules: {
-    'never-device-emitter-remove-all': require('./lib/rules/never-device-emitter-remove-all')
+    'never-device-emitter-remove-all': require('./lib/rules/never-device-emitter-remove-all'),
+    'no_disable_yellow_box': require('./lib/rules/no_disable_yellow_box')
   },
   configs: {
     recommended: {
       rules: {
-        'never-device-emitter-remove-all': 2
+        'never-device-emitter-remove-all': 2,
+        'no_disable_yellow_box': 2
       }
     }
   }
